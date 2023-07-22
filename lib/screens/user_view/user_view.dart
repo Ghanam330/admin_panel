138d1686c88c2605649e1bd6befc7567ec4a409b
Ahmed <yghnam334455@gmail.com>
2023-07-23 02:34:01 +0300

    finish

--- lib/screens/user_view/user_view.dart
+++ lib/screens/user_view/user_view.dart
@@ -1,5 +1,6 @@
 import 'package:admin_panel/constants/colors.dart';
 import 'package:admin_panel/provider/app_provider.dart';
+import 'package:admin_panel/screens/chat_screen/chat_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -21,63 +22,70 @@ class UserView extends StatelessWidget {
             itemCount: value.userList.length,
             itemBuilder: (context, index) {
               UserModel userModel = value.userList[index];
-              return Card(
-                color: Colors.white,
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Row(
-                      children: [
-                    userModel.image != null
-                        ? Image.network(
-                            userModel.image!,
-                          )
-                        : const CircleAvatar(
-                            backgroundColor: kPrimaryGreen,
-                            child: Icon(Icons.person, color: Colors.white),
+              return InkWell(
+                onTap: (){
+                  Navigator.push(context,
+                      MaterialPageRoute(
+                          builder: (context)=>ChatScreen(uid: userModel.id,)));
+                },
+                child: Card(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Row(
+                        children: [
+                      userModel.image != null
+                          ? Image.network(
+                              userModel.image!,
+                            )
+                          : const CircleAvatar(
+                              backgroundColor: kPrimaryGreen,
+                              child: Icon(Icons.person, color: Colors.white),
+                            ),
+                      const SizedBox(width: 10),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            userModel.name,
+                            style: const TextStyle(),
                           ),
-                    SizedBox(width: 10),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          userModel.name,
-                          style: const TextStyle(),
-                        ),
-                        Text(
-                          userModel.phone,
-                          style: const TextStyle(),
-                        ),
+                          Text(
+                            userModel.phone,
+                            style: const TextStyle(),
+                          ),
 
-                      ],
+                        ],
 
-                    ),
-                    const Spacer(),
-                    value.getIsDeletingLoading
-                        ? CircularProgressIndicator()
-                        : CupertinoButton(
-                            padding: EdgeInsets.zero,
+                      ),
+                      const Spacer(),
+                      value.getIsDeletingLoading
+                          ? const CircularProgressIndicator()
+                          : CupertinoButton(
+                              padding: EdgeInsets.zero,
+                              onPressed: () {
+                                value.deleteUser(userModel);
+                              },
+                            child: IconButton(
                             onPressed: () {
                               value.deleteUser(userModel);
                             },
-                          child: IconButton(
-                          onPressed: () {
-                            value.deleteUser(userModel);
-                          },
-                          icon: Icon(Icons.delete, color: Colors.red)),
-                        ),
-                        // SizedBox(width: 6.0),
-                        // GestureDetector(
-                        //   // padding: EdgeInsets.zero,
-                        //   onTap: () {
-                        //     value.deleteUser(userModel);
-                        //   },
-                        //   child: IconButton(
-                        //       onPressed: () {
-                        //         value.deleteUser(userModel);
-                        //       },
-                        //       icon: Icon(Icons.edit)),
-                        // ),
-                  ]),
+                            icon: const Icon(Icons.delete, color: Colors.red)),
+                          ),
+                          // SizedBox(width: 6.0),
+                          // GestureDetector(
+                          //   // padding: EdgeInsets.zero,
+                          //   onTap: () {
+                          //     value.deleteUser(userModel);
+                          //   },
+                          //   child: IconButton(
+                          //       onPressed: () {
+                          //         value.deleteUser(userModel);
+                          //       },
+                          //       icon: Icon(Icons.edit)),
+                          // ),
+                    ]),
+                  ),
                 ),
               );
             },
